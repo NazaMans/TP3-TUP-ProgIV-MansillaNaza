e81c23df3bbd7ec6f0475bf471b7483d196ea16c
Nazareno Exequiel Mansilla <mansillanaza@gmail.com>
2025-11-12 22:06:28 -0300

    Arreglo index.js porque no andaba

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import { conectarDB } from "./db.js";
+
+import usuarioRouter from "./usuario.js"
+
+conectarDB();
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.use(cors());
+
+
+app.get("/", (req, res) => {
+    res.send("TP3 de programacion IV")
+});
+
+app.use("/usuarios", usuarioRouter);
+
+app.listen(port, () => {
+    console.log("La aplicacion esta andando en el puerto 3000, lo quiero profe :)")
+})
