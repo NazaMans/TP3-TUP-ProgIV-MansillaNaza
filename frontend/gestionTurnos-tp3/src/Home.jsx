aa1524ed0eeebfdc58c7af8719d6f65d4de1972e
Nazareno Exequiel Mansilla <mansillanaza@gmail.com>
2025-11-14 13:33:06 -0300

    Ultimo commit "personalizando" pagina home

--- frontend/gestionTurnos-tp3/src/Home.jsx
+++ frontend/gestionTurnos-tp3/src/Home.jsx
@@ -1,14 +1,21 @@
+import { useAuth } from './auth/auth';
 
 function Home() {
- 
+  
+  const { nombre, apellido, isAuthenticated } = useAuth();
 
   return (
-    <>
-      <div>
-        <p>Hola profe</p>
-      </div>
-    </>
-  )
+    <article>
+
+      {isAuthenticated ? (
+        <h1>
+          Bienvenido {nombre} {apellido}
+        </h1>
+      ) : (
+        <h1>Hola profe</h1>
+      )}
+    </article>
+  );
 }
 
-export default Home
+export default Home;
